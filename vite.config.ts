957737964a54bc5584422a713cd121c7d74d0405
gurgen <gaaegiazaryan@gmail.com>
2025-11-13 23:15:59 +0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue"
 import path from "node:path"
 
 export default defineConfig({
-  base: "/crypto-dashboard/",
+  base: "/crypto-dashboard-test/",
   plugins: [vue()],
   resolve: {
     alias: {
